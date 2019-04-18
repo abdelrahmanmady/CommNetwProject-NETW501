dc7aaa16ee8eef913b977e9ba66b63612022ccb8
Abdelrhman Mostafa Mady <40574826+abdelrahmanmady@users.noreply.github.com>
2019-04-18 16:15:12 +0200

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -1,6 +1,6 @@
 import java.io.*; 
 import java.net.*; 
-public class TCPClient {
+class TCPClient {
 
 	public static void main(String argv[]) throws Exception 
 	{ 
@@ -9,11 +9,12 @@ public class TCPClient {
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
 		Socket clientSocket = new Socket("localhost", 6789);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
-        BufferedReader inFromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine(); 
-        outToServer.writeBytes(sentence + '\n'); 
-        modifiedSentence = inFromServer.readLine(); 
-        System.out.println("FROM SERVER: " + modifiedSentence); 
-        clientSocket.close();  
+                BufferedReader inFromServer =  new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                sentence = inFromUser.readLine(); 
+                outToServer.writeBytes(sentence + '\n'); 
+                modifiedSentence = inFromServer.readLine(); 
+                System.out.println("FROM SERVER: " + modifiedSentence); 
+                clientSocket.close();  
 	}
 }
+
